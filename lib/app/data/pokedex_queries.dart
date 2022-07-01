6ca1c635b179e712fba69585da65692fcea8b2e6
angelo-squadra <angelo.savioti@squadra.com.br>
2022-07-01 05:01:24 -0300

    created query

--- lib/app/data/pokedex_queries.dart
+++ lib/app/data/pokedex_queries.dart
@@ -1,13 +1,55 @@
 class PodekexQueries {
-  static String get getPokemon {
+  static String getPokemon(String pokemonName) {
     return """
-    query {
-      allArtists {
-            id
-            name
-            imageUrl
-            description
-        }
+    query getPokemon(pokemon: $pokemonName, reverseFlavorTexts: true, takeFlavorTexts: 1,) {
+      abilities {
+        first
+        second
+        hidden
+        special
+      }
+      baseStats {
+        attack
+        defense
+        hp
+        specialattack
+        specialdefense
+        speed
+      }
+      baseStatsTotal
+      bulbapediaPage
+      dexNumber
+      evolutionLevel
+      evolutions
+      evYields {
+        attack
+        defense
+        hp
+        specialattack
+        specialdefense
+        speed
+      }
+      flavorTexts {
+        game
+        flavor
+      }
+      forme
+      gender {
+        female
+        male
+      }
+      height
+      isEggObtainable
+      levellingRate
+      maximumHatchTime
+      minimumHatchTime
+      preevolutions
+      serebiiPage
+      shinySprite
+      species
+      sprite
+      types
+      weight
     }
    """;
   }
